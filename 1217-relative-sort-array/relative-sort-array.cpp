5db47366a9559761eb369f1f03857d0c61bf7af0
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2024-06-11 10:47:30 +0530

    Time: 5 ms (34.15%) | Memory: 9.4 MB (91.48%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        /*
         vector<int> output;
 
         int index=0;
@@ -21,5 +22,36 @@ public:
         }
 
         return output;
+        */
+
+        /*
+        unordered_map<int,int> map;
+        vector<int> output;
+
+        for(size_t i=0;i<arr1.size();i++){
+            int n=arr1[i];
+            map[n]++;
+        }
+
+        for(int i=0;i<arr2.size();i++){
+            if(map.count(arr2[i])){
+                for(int j=0;j<)
+            }
+        }
+        */
+
+        int a=0;
+
+        for(int i =0; i< arr2.size(); i++){ 
+            for(int j= 0; j<arr1.size();j++){      
+                if(arr1[j]==arr2[i]){
+                    swap(arr1[a],arr1[j]);
+                    a++;
+                }  
+            }
+        }
+        sort(arr1.begin()+a, arr1.end());
+        return arr1;
+        
     }
 };
