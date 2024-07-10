a7bb29b470b89b0d36e1a46cc1fa766258443e54
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2024-07-10 22:17:06 +0530

    Time: 4 ms (67.13%) | Memory: 13.9 MB (84.88%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -4,12 +4,10 @@ public:
         int output= 0;
 
         for(int i=0;i<logs.size();i++){
-            if(logs[i]=="../" && output!=0){
+            if(logs[i]=="../" && output!=0)
                 output--;
-            }
-            else if(logs[i]!="./" && logs[i]!="../"){
+            else if(logs[i]!="./" && logs[i]!="../")
                 output++;
-            }
         }
 
         return output;
