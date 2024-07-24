47edcfc7e2c18fd068a049407b1e6d425bf122f3
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2024-07-24 23:36:12 +0530

    Time: 179 ms (80.09%) | Memory: 80.6 MB (90.04%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int, int>> val(n, pair<int, int>(0, 0));
+        vector<pair<int, int>>val(n, pair<int,int>(0,0));
 
-        for (int i = 0; i < n; ++i) { 
+        for (int i=0; i<n; i++) { 
             int num = nums[i], j = 0, ans = 0;
             if (num == 0) {
                 ans = mapping[num];
             } else {
                 while (num > 0) {
-                    ans += pow(10, j) * mapping[num % 10];
+                    ans += pow(10, j)*mapping[num % 10];
                     num /= 10;
-                    ++j;
+                    j++;
                 }
             }
             val[i].first = ans;
