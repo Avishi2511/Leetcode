ae91e5cd9516733131a56ef3c9b8bcd7460eb6a2
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-06-11 13:09:15 +0530

    Time: 14 ms (21.78%) | Memory: 7.7 MB (87.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
+
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        return fib(n-1)+fib(n-2);
+        /*
         vector<int> dp(31,-1);
         dp[0] = 0;
         dp[1] = 1;
@@ -8,5 +14,6 @@ public:
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
+        */
     }
 };
