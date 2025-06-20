abb8ae164c304fa082f7f329b51e84c55d66752d
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-06-20 12:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (99.53%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
+        /*
         int maxm=0, ones=0;
 
         for(int i=0;i<nums.size();i++){
@@ -11,5 +12,23 @@ public:
             else ones=0;
         }
         return maxm;
+        */
+
+        //Brute
+        int count = 0;
+        int maxCount = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+                if(count>maxCount){
+                    maxCount = count;
+                }
+            }
+            else {
+                count=0;
+            }
+        }
+
+        return maxCount;
     }
 };
