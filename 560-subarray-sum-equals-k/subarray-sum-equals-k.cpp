b712a7a0b9cc7a8255894818b1d763f9e3afbc5e
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-06-30 15:21:38 +0530

    Time: 52 ms (50.88%) | Memory: 51 MB (19.53%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        //CAN'T USE SLIDING WINDOW IN THIS!!!!
+
+        //Brute
+        /*
+        int cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            int sum = 0;
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+                if(sum==k){
+                    cnt++;
+                }
+                cout<<sum<<endl;
+            }
+        }
+
+        return cnt;
+        */
+
+
+        unordered_map<int,int> map;
+        map[0] = 1;
+        int prefixSum = 0;
+        int cnt = 0;
+
+        for(int i=0;i<nums.size();i++){
+            prefixSum += nums[i];
+            int remove = prefixSum - k;
+            cnt += map[remove];
+            map[prefixSum] += 1;
+        }
+
+        return cnt;
+
+
+    }
+};
