27ac320cc7029d8e07740bf716545f41540ecf17
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-07-02 12:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (59.53%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -28,32 +28,56 @@ public:
         return ans;
         */
 
-    stack<int> st;
-    vector<int> ans;
-    int n1 = nums1.size();
-    int n2 = nums2.size();
+        //Better
+        /*
+        stack<int> st;
+        vector<int> ans;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-    for (int i = 0; i < n1; i++) {
-        int j = 0;
-        while (j < n2 && nums2[j] != nums1[i]) {
-            j++;
-        }
-        j++;
-        while (j < n2) {
-            if (nums2[j] > nums1[i]) {
-                st.push(nums2[j]);
-                break;
+        for (int i = 0; i < n1; i++) {
+            int j = 0;
+            while (j < n2 && nums2[j] != nums1[i]) {
+                j++;
             }
             j++;
+            while (j < n2) {
+                if (nums2[j] > nums1[i]) {
+                    st.push(nums2[j]);
+                    break;
+                }
+                j++;
+            }
+
+            if (st.empty()) ans.push_back(-1);
+            else ans.push_back(st.top());
+
+            while (!st.empty()) st.pop();
         }
 
-        if (st.empty()) ans.push_back(-1);
-        else ans.push_back(st.top());
+        return ans;
+        */
 
-        while (!st.empty()) st.pop();
-    }
+        //Optimal
 
-    return ans;
+        unordered_map<int, int> map;
+        stack<int> st;
 
+        for (int i=0;i<nums2.size();i++) {
+            while (!st.empty() && nums2[i] > st.top()) {
+                map[st.top()] = nums2[i];
+                st.pop();
+            }
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for (int i=0;i<nums1.size();i++) {
+            if (map.count(nums1[i]))
+                ans.push_back(map[nums1[i]]);
+            else
+                ans.push_back(-1);
+        }
+        return ans;
     }
 };
