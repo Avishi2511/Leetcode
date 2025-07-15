6e2fdd43cff7b4ea5ff7865feaacbeee353186fe
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-07-15 14:17:41 +0530

    Time: 37 ms (39.42%) | Memory: 64.7 MB (9.05%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -3,8 +3,10 @@ public:
     int findKthLargest(vector<int>& nums, int k) {
         
         //Naive
+        /*
         sort(nums.begin(), nums.end(), greater<int>());
         return nums[k-1];
+        */
 
         //Using heap
 
@@ -18,6 +20,7 @@ public:
         while(i!=k){
             klargest = pq.top();
             pq.pop();
+            i++;
         }
 
         return klargest;
