e0ad04005d345f54c0413b927140dd54beb57fef
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-08-02 10:55:02 +0530

    Time: 1 ms (17.73%) | Memory: 12 MB (6.19%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int path(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
-        if (m < 0 || n < 0) return 0;
+        if (m < 0 || n < 0) return 0; //this condition was added since even in first row and column
+        // there can be an obstacle, and so the base case is only 1 which is the starting [0][0] cell
+        
         if (obstacleGrid[m][n] == 1) return 0;
 
-        if (m == 0 && n == 0) return 1;
+        if (m == 0 && n == 0) return 1; //base case (starting cell not complete first row and column)
 
         if (dp[m][n] != -1) return dp[m][n];
 
