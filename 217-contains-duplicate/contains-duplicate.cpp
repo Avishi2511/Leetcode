22ad96784255164c274bd61e4de3b28d1724936a
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-08-27 10:37:10 +0530

    Time: 55 ms (39.25%) | Memory: 90.8 MB (47.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -6,11 +6,14 @@ public:
 
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
+            if(mp[nums[i]]>1) return true;
         }
 
+        /*
         for(const auto &p : mp){
             if(p.second>1) return true;
         }
+        */
 
         return false;
     }
