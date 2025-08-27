c76db4caee0000b5c08b7cacda066a420c627892
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-08-27 19:02:03 +0530

    Time: 59 ms (5.00%) | Memory: 65 MB (5.06%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    /*
     void dfs(int node, vector<vector<int>> &adjLs, vector<int> &vis){
         vis[node] = 1;
         for(auto it : adjLs[node]){
@@ -8,8 +9,44 @@ private:
             }
         }
     }
+    */
 public:
+    void dfs(int node, vector<vector<int>> adjList, vector<int> &vis){
+        vis[node] = 1;
+        for(auto it : adjList[node]){
+            if(vis[it]==0){
+                dfs(it,adjList,vis);
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<vector<int>> adjList(V);
+
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(i!=j && isConnected[i][j]==1){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
+            }
+        }
+
+        vector<int> visited(V,0);
+        int cnt = 0;
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                cnt++;
+                dfs(i,adjList,visited);
+            }
+        }
+
+        return cnt;
+
+
+
+
+        /*
         int V = isConnected.size();
         vector<vector<int>> adjLs(V);
 
@@ -33,5 +70,41 @@ public:
         }
 
         return cnt;
+        */
     }
+
 };
+
+
+
+//TOPOLIGAL SORT (UNRELATED)
+
+    /*
+  void dfs(int node, int vis[], stack<int> st, vector<int> adj){
+        vis[node] = 1;
+        for(auto it : adj[node]){
+            if(!vis[it]) dfs(it, vis, st, adj);
+        }
+        st.push(node);
+    }
+
+
+    vector<int> topoSort(int V, vector<int> adj[]){
+        int vis[V] = {0};
+        stack<int> st;
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                st.push(vis[i]);
+                dfs(i, vis, st);
+            }
+        }
+
+        vector<int> ans;
+        while(!st.empty()){
+            int s = st.pop();
+            ans.push_back(s);
+        }
+
+        return ans;
+    }
+    */
