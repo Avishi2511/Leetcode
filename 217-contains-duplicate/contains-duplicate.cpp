a7a9617f4aa28b05c3468a0ef291dd1c73c44453
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-08-27 11:08:04 +0530

    Time: 26 ms (81.59%) | Memory: 71.1 MB (79.68%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,11 +3,18 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         
         //unordered_map<int,int> mp;
+        sort(nums.begin(), nums.end()); 
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i-1]) return true;
+        }
+        return false;
+        /*
         unordered_set<int> mp;
         for(int i=0;i<nums.size();i++){
             if(mp.count(nums[i])) return true;
             mp.insert(nums[i]);
         }
+        */
         /*
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
