37f6c4e8c38b020f7233a8cf6ac74a85d60eb426
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-09-04 16:13:59 +0530

    Time: 1805 ms (5.25%) | Memory: 488 MB (5.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -9,17 +9,12 @@ public:
     }
     int countSubstrings(string s) {
         int count = s.size();
-        vector<string> store;
         for(int i=0;i<s.size();i++){
             string str;
             for(int j=i+2;j<=s.size();j++){
                 str = s.substr(i,(j-i));
-                store.push_back(str);
+                if(checkPalindrome(str)==true) count++;
             }
-        }
-
-        for(int i=0;i<store.size();i++){
-            if(checkPalindrome(store[i])==true) count++;
         }
 
         return count;
