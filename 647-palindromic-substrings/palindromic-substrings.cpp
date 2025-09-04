cf4aa7c4fbce6fcfd53dbfd83e710c5fb6bb7511
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-09-04 16:50:24 +0530

    Time: 3 ms (88.98%) | Memory: 8.6 MB (68.00%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -8,15 +8,38 @@ public:
         return true;
     }
     int countSubstrings(string s) {
+        /*
         int count = s.size();
+
         for(int i=0;i<s.size();i++){
             string str;
             for(int j=i+2;j<=s.size();j++){
                 str = s.substr(i,(j-i));
-                if(checkPalindrome(str)==true) count++;
+                if(checkPalindrome(str)==true){ 
+                    count++;
+                }
             }
         }
 
         return count;
+        */
+
+        int n=s.size(), ans=0;
+
+        auto expand = [&](int L, int R) {
+            int cnt = 0;
+            while (L>=0 && R<n && s[L]==s[R]) {
+                ++cnt; 
+                --L; 
+                ++R;
+            }
+            return cnt;
+        };
+
+        for (int i=0; i<n; ++i) {
+            ans += expand(i, i);     
+            ans += expand(i, i+1); 
+        }
+        return ans;
     }
 };
