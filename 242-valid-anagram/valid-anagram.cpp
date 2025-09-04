a2e22583c617fb9ab5335b48a34ef7cc543e739e
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-09-04 08:26:40 +0530

    Time: 205 ms (8.14%) | Memory: 9.7 MB (48.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        /* Brute
         if(s.size()!=t.size()) return false;
         
         sort(s.begin(),s.end());
@@ -10,6 +11,32 @@ public:
             if(s[i]==t[i]) continue;
             else return false;
         }
+        return true;
+        */
+
+        //Optimized
+
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]++;
+        }
+
+        for(int i=0;i<s.size();i++){
+            if(mp.count(s[i])>=1){
+                mp[s[i]]--;
+            }
+            else{
+                break;
+            }
+            cout<<mp[t[i]]<<endl;
+        }
+
+        for (auto &p : mp) {
+            if (p.second >= 1) return false;
+        }
+
         return true;
     }
 };
