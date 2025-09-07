c32be62e9fb6d84bf834e997ba54ea5e884eae88
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-09-07 12:23:58 +0530

    Time: 31 ms (35.03%) | Memory: 88.3 MB (69.12%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) { 
+        long long sum = 0;
+        int maxm = INT_MIN;
+        for(int i=0;i<candies.size();i++){
+            sum += candies[i];
+            maxm = max(maxm,candies[i]);
+        }
+        if(sum<k) return 0;
+
+        int low = 1, high = maxm;
+
+        int mid, ans=0;
+        while(low<=high){
+            mid = low+(high-low)/2;
+
+            long long piles = 0;
+            for(int i=0;i<candies.size();i++){
+                piles += candies[i]/mid;
+            }
+
+            if(piles >= k) {
+                ans = mid;
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+
+        return ans;
+    }
+};
