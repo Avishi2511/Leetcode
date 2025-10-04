8fa050cbb3dd09de9463c631d2765469b7bf5f2e
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2025-10-04 11:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (7.11%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -19,6 +19,7 @@ public:
         return minLen;
         */
 
+        /*
         int sum = 0, len = 0;
         int minLen = INT_MAX;
         int l = 0, r = 0;
@@ -31,12 +32,29 @@ public:
                 sum -= nums[l];
                 l++;
             }
-            
+
             r++;
         }
 
         if(minLen == INT_MAX) return 0;
         return minLen;
-        
+        */
+
+
+        int n = nums.size();
+        int l = 0, sum = 0;
+        int minLen = INT_MAX;
+
+        for (int r = 0; r<n; r++) {
+            sum += nums[r];
+
+            while (sum >= target) {
+                minLen = min(minLen, r-l+1);
+                sum -= nums[l++];
+            }
+        }
+
+        return (minLen == INT_MAX) ? 0 : minLen;
+            
     }
 };
