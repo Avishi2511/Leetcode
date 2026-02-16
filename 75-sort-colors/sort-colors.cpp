99e7bb041eed0b277b1619e5ea5d314b55696a07
Avishi Mittal <118925593+Avishi2511@users.noreply.github.com>
2026-02-16 23:30:16 +0530

    Time: 2 ms (3.18%) | Memory: 11.6 MB (77.72%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        /*
         int n=nums.size();
         for(int i=0;i<n-1;i++){
             int min=nums[i];
@@ -13,5 +14,18 @@ public:
             }
             swap(nums[k], nums[i]);
         }
+        */
+
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+
+        for(int i=0;i<mp[0];i++) nums[i] = 0;
+
+        for(int i=mp[0];i<mp[0]+mp[1];i++) nums[i] = 1;
+
+        for(int i=mp[0]+mp[1];i<mp[0]+mp[1]+mp[2];i++) nums[i] = 2;
     }
 };
